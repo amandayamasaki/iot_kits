26a5c18d48c1772654cf5e4270650577336549c1
Amanda Yamasaki <amandayamasaki@u.northwestern.edu>
2018-04-14 19:42:53 -0500

    updated index

---
+++ index.js
@@ -0,0 +1,37 @@
+var Particle = require('particle-api-js');
+var particle = new Particle();
+var keys = require('./keys.json');
+
+var deviceOneId = keys.ID_ONE;
+var authCode = keys.AUTH_CODE;
+var username = keys.USERNAME;
+var password = keys.PASS;
+
+var token;
+
+particle.login({username: username, password: password}).then(
+    function(data) {
+        token = data.body.access_token;
+        console.log("Log in successful")
+
+    },
+    function (err) {
+        console.log('Could not log in.', err);
+    }
+);
+
+function testFunction(){
+    var fnPr = particle.callFunction({  deviceId: deviceOneId, 
+                                        name: 'int_fun', 
+                                        argument: 'wifi_rssi', 
+                                        auth: authCode });
+
+    fnPr.then(
+        function(data) {
+            console.log('Function called succesfully:', data);
+        }, function(err) {
+            console.log('An error occurred:', err);
+        }
+    );
+}
+
